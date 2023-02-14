68c79ba1433b972082cd6467c046e1f032d85cea
yonatanaltman <yonatan@yaltman.com>
2023-02-14 22:10:01 +0200

    clean code

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ console.log("Hello, TypeScript and Webpack!");
 
 
 import './who-we-are/who-we-are';
+import './navigation/navigation';
---
+++ src/navigation/navigation.ts
@@ -0,0 +1,12 @@
+const wrapper = (document.getElementsByClassName('navMenu')[0] as HTMLDivElement);// get initial position of the element
+const PADDING = 10;
+
+window.addEventListener('scroll', (event) => {                  // assign scroll event listener
+    const screenTop = (event.currentTarget as Window).scrollY;
+    if (screenTop >= wrapper.offsetTop + PADDING) {           // apply position: fixed if you
+        wrapper.classList.add('sticky');
+    } else {                                   // apply position: static
+        wrapper.classList.remove('sticky');
+    }
+
+});
