3b0e5753e25e4c85bab25aae85e8a6d76c99cb51
yonatanaltman <yonatan@yaltman.com>
2023-02-15 15:52:52 +0200

    add products.ts dialog

--- src/products/products.ts
+++ src/products/products.ts
@@ -1,5 +1,6 @@
 import './products.scss';
 import {createProduct, Product} from "./product.interface";
+import {MDCDialog} from '@material/dialog';
 
 const products: Partial<Product>[] = [
     createProduct('p000012', 'Advance Rxjs'),
@@ -9,9 +10,59 @@ const products: Partial<Product>[] = [
     createProduct('p000016', 'Fullstack workshop'),
     createProduct('p000017', 'Advance Typescript'),
 ]
+
+const dialogTemplate = `<div class="mdc-dialog">
+  <div class="mdc-dialog__container">
+    <div class="mdc-dialog__surface"
+      role="alertdialog"
+      aria-modal="true"
+      aria-labelledby="my-dialog-title"
+      aria-describedby="my-dialog-content">
+      <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
+      <h2 class="mdc-dialog__title" id="my-dialog-title"><!--
+     -->Choose a Ringtone<!--
+   --></h2>
+      <div class="mdc-dialog__content" id="my-dialog-content">
+        <ul class="mdc-deprecated-list mdc-deprecated-list--avatar-list">
+          <!-- ... -->
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="mdc-dialog__scrim"></div>
+</div>`;
+document.getElementById('products-dialog').innerHTML = dialogTemplate;
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
+
+
+const getLIElement = (p: Partial<Product>, index: number): HTMLLIElement => {
+    const li = document.createElement('li');
+    li.innerHTML = `     <li class="mdc-deprecated-list-item" tabindex="${index}" data-mdc-dialog-action="none">
+            <span class="mdc-deprecated-list-item__text">${p.name}</span>
+          </li>
+     `;
+    return li;
+}
+
 const onClick = (event: Event) => {
-    console.log('Our products',products);
+    console.log('Our products', products);
+    const ul = document.createElement('ul');
+    ul.setAttribute('id','my-prod-list')
+    products.forEach((p, i) => {
+        ul.appendChild(getLIElement(p, i))
+
+    })
+    document.getElementById('my-dialog-content').appendChild(ul);
+    dialog.open();
+
+
+    dialog.listen('MDCDialog:closed', () => {
+        document.getElementById('my-dialog-content').removeChild(document.getElementById('my-prod-list'));
+    });
 
 }
 
+
 document.getElementById('products-btn').addEventListener('click', onClick);
+
+
